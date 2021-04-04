54dcd1cc672b1ed702b3bb9c85e54f4837fb9c18
JhsK <qwe6293@nate.com>
2021-04-04 20:27:53 +0900

    reducer 구현

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Styled from "styled-components";
 import PropTypes from "prop-types";
 import useInput from "./hooks/useInput";
+import { useDispatch } from "react-redux";
+import { loginAction } from "../reducers/index";
 
 const ButtonWrapper = Styled.div`
     margin-top: 10px;
@@ -13,13 +15,15 @@ const FormWrapper = Styled(Form)`
     padding: 10px;
 `;
 
-const LoginForm = ({ setIsLoggedIn }) => {
+const LoginForm = () => {
+  const dispatch = useDispatch();
   const [id, onChangeId] = useInput("");
   const [password, onChangePassword] = useInput("");
 
   const onSubmitForm = useCallback(() => {
     console.log(id, password);
     setIsLoggedIn(true);
+    dispatch(loginAction({ id, password }));
   }, [id, password]);
 
   return (
